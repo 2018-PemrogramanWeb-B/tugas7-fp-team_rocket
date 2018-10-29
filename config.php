65096cab9de19ca65137691dd1bc9ab1679165c7
bosmobosmo <36439009+bosmobosmo@users.noreply.github.com>
2018-10-29 19:32:45 +0700

    registrasi sederhana

--- config.php
+++ config.php
@@ -5,12 +5,12 @@ $username = "root";
 $password = "";
 $database = "myjon";
 
-$conn=mysql_connect($servername,$username,$password);
-$a=mysql_select_db($database, $conn);
+$conn=new mysqli($servername, $username, '', $database);
+// $a=mysqli_select_db($database, $conn);
 
 if(!$conn) {
 	echo "failed";
-	die("Connection failed: " . mysql_error());	
+	die("Connection failed: " . mysql_error());
 }
 
 ?>
