ca0f4c11a17cb40d53a88bf4537d2cdcd8aab6d8
rigold <rigold95@gmail.com>
2018-12-18 19:22:26 +0700

    Login&Register Fix

--- home.php
+++ home.php
@@ -1,33 +1,105 @@
 <?php
-    include("config.php");
-    session_start();
-
-    function greet($username) {
-        date_default_timezone_set('Asia/Jakarta');
-        $time = date('H');
-        if ($time < "12") {
-            echo "Selamat Pagi $username!!";
-        } else
-        if ($time >= "12" && $time < "17") {
-            echo "Selamat Siang $username!!";
-        } else
-        if ($time >= "17" && $time < "19") {
-            echo "Selamat Sore $username!!";
-        } else
-        if ($time >= "19") {
-            echo "Selamat Malam $username!!";
-        }
-    }
-
-    if ($_SESSION["username"]){
-        greet($_SESSION["username"]);
-    } else {
-        header('location:index.php');
-    }
+  include("config.php");
+  session_start();
 ?>
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>MyJon</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <style>
+  .fakeimg {
+      height: 200px;
+      background: #aaa;
+  }
+  </style>
+</head>
 <body>
-<button formaction="logout.php">Log Out</button>
+    <form action="index.php" method="get">
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>MyJon</h1>
+  <p>FP PWEB B</p> 
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+    <?php
+      if ($_SESSION){
+        echo"<p>$_SESSION[username]<p>
+        <li class='nav-item'>
+        <a class='nav-link' href='logout.php'>Logout</a>
+      </li> ";
+    } else {
+        echo"<li class='nav-item'>
+        <a class='nav-link' href='login.php'>Login</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link' href='register.php'>Register</a>
+      </li>";
+    }
+    ?>
+      <li class="nav-item">
+        <a class="nav-link" href="config.php">Config</a>
+      </li>   
+      
+    </ul>
+  </div>  
+</nav>
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>About Me</h2>
+      <h5>Photo of me:</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+      <h3>Some Links</h3>
+      <p>Lorem ipsum dolor sit ame.</p>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-8">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Dec 7, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <br>
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+  </div>
+</div>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>Footer</p>
+</div>
+
 </body>
 </html>
--- register.php
+++ register.php
@@ -27,53 +27,213 @@
       $query = "INSERT INTO user (username, password) VALUES ('$username', '$password')";
       mysqli_query($conn, $query);
       $_SESSION[username] = $username;
-      header('location:index.php');
+      header('location:home.php');
     }
   }
 ?>
-<html>
 
-   <head>
-      <title>Registration Page</title>
+<!DOCTYPE html>
 
-      <style type = "text/css">
-         body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-         }
-         label {
-            font-weight:bold;
-            width:100px;
-            font-size:14px;
-         }
-         .box {
-            border:#666666 solid 1px;
-         }
-      </style>
+<html >
+    <head>
+        <meta charset="UTF-8">
+        <title>MyJourney</title>
+        <link rel="icon" href="images/favicon.png">
 
-   </head>
+        <style>
+            @import url(https://fonts.googleapis.com/css?family=Exo:100,200,400);
+            @import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
+            
+            body{
+                margin: 0;
+                padding: 0;
+                background: #fff;
+                color: #fff;
+                font-family: Arial;
+                font-size: 12px;
+            }
 
-   <body bgcolor = "#FFFFFF">
+            .body{
+                position: absolute;
+                top: -20px;
+                left: -20px;
+                right: -40px;
+                bottom: -40px;
+                width: auto;
+                height: auto;
+                background-image: url(images/bg.jpg);
+                background-size: cover;
+                -webkit-filter: blur(5px);
+                z-index: 0;
+            }
 
-      <div align = "center">
-         <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Register</b></div>
+            .grad{
+                position: absolute;
+                top: -20px;
+                left: -20px;
+                right: -40px;
+                bottom: -40px;
+                width: auto;
+                height: auto;
+                background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(100%,rgba(0,0,0,0.65))); /* Chrome,Safari4+ */
+                z-index: 1;
+                opacity: 0.7;
+            }
 
-            <div style = "margin:30px">
+            .header{
+                position: absolute;
+                top: calc(50% - 35px);
+                left: calc(50% - 255px);
+                z-index: 2;
+            }
 
-               <form action = "" method = "post">
-                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-                  <input type = "submit" value = " Submit "/><br />
-               </form>
+            .header div{
+                float: left;
+                color: #fff;
+                font-family: 'Exo', sans-serif;
+                font-size: 35px;
+                font-weight: 200;
+            }
 
-               <div style = "font-size:11px; color:#cc0000; margin-top:10px"></div>
+            .header div span{
+                color: #5379fa !important;
+            }
 
-            </div>
+            .login{
+                position: absolute;
+                top: calc(50% - 75px);
+                left: calc(50% - 50px);
+                height: 150px;
+                width: 350px;
+                padding: 10px;
+                z-index: 2;
+            }
 
-         </div>
+            .login input[type=text]{
+                width: 250px;
+                height: 30px;
+                background: transparent;
+                border: 1px solid rgba(255,255,255,0.6);
+                border-radius: 2px;
+                color: #fff;
+                font-family: 'Exo', sans-serif;
+                font-size: 16px;
+                font-weight: 400;
+                padding: 4px;
+            }
 
-      </div>
+            .login input[type=password]{
+                width: 250px;
+                height: 30px;
+                background: transparent;
+                border: 1px solid rgba(255,255,255,0.6);
+                border-radius: 2px;
+                color: #fff;
+                font-family: 'Exo', sans-serif;
+                font-size: 16px;
+                font-weight: 400;
+                padding: 4px;
+                margin-top: 10px;
+            }
 
-   </body>
+            .login input[type=submit]{
+                width: 260px;
+                height: 35px;
+                background: #fff;
+                border: 1px solid #fff;
+                cursor: pointer;
+                border-radius: 2px;
+                color: #a18d6c;
+                font-family: 'Exo', sans-serif;
+                font-size: 16px;
+                font-weight: 400;
+                padding: 6px;
+                margin-top: 10px;
+            }
+
+            .login input[type=submit]:hover{
+                opacity: 0.8;
+            }
+
+            .login input[type=submit]:active{
+                opacity: 0.6;
+            }
+
+            .login input[type=submit]:focus{
+                outline: none;
+            }
+
+            .login input[type=button]{
+                width: 260px;
+                height: 35px;
+                background: #fff;
+                border: 1px solid #fff;
+                cursor: pointer;
+                border-radius: 2px;
+                color: #a18d6c;
+                font-family: 'Exo', sans-serif;
+                font-size: 16px;
+                font-weight: 400;
+                padding: 6px;
+                margin-top: 10px;
+            }
+
+            .login input[type=button]:hover{
+                opacity: 0.8;
+            }
+
+            .login input[type=button]:active{
+                opacity: 0.6;
+            }
+
+            .login input[type=button]:focus{
+                outline: none;
+            }
+
+            .login input[type=text]:focus{
+                outline: none;
+                border: 1px solid rgba(255,255,255,0.9);
+            }
+
+            .login input[type=password]:focus{
+                outline: none;
+                border: 1px solid rgba(255,255,255,0.9);
+            }
+
+            ::-webkit-input-placeholder{
+               color: rgba(255,255,255,0.6);
+            }
+
+            ::-moz-input-placeholder{
+               color: rgba(255,255,255,0.6);
+            }
+        </style>
+    </head>
+
+    <body>
+      <div class="body"></div>
+        <div class="grad"></div>
+        <div class="header">
+            <div>My<span>Journey</span></div><br>
+            <div>Register</div>
+        </div>
+        <br>
+        <div  class="login" >
+            <table align="center" border="0" cellpadding="0" cellspacing="1" width="300">
+                <tr>
+                    <td>
+                        <form action = "" method = "post">
+                            <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                            <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                            <input type = "submit" value = " Submit "/><br />
+                        </form>
+                        
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </body>
+
 </html>
+
+<?php mysql_close($conn); ?>
